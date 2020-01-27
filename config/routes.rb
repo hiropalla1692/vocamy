a19ae816b3c049bbb542af7c3e3dbb0ee967505e
Hirokuni <user@USERnoMacBook.lan>
2020-01-27 19:30:05 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'vocas#index'
-  get '*path', to: 'vocas#index'
   resources :vocas
-
+  resources :users
+  get 'signup'  => 'users#new'
 end
