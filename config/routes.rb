14cd85b7ece39df685dacfd471773efc3d071b4f
Hirokuni <user@USERnoMacBook.lan>
2020-01-27 21:05:27 +0900

    finish login logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'vocas#index'
+  get 'signup'  => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :vocas
   resources :users
-  get 'signup'  => 'users#new'
 end
